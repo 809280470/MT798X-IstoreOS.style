17f32289391487bb61ea5085ea3b501d81ccbe48
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-01-29 22:49:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,30 +9,31 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-
 # istore
-git clone --depth=1 https://github.com/linkease/istore package/istore
-git clone --depth=1 https://github.com/linkease/nas-packages package/nas
-git clone --depth=1 https://github.com/linkease/nas-packages-luci package/nas-luci
+git clone --depth=1 https://github.com/linkease/istore  package/istore
+git clone --depth=1 https://github.com/linkease/nas-packages  package/nas
+git clone --depth=1 https://github.com/linkease/nas-packages-luci  package/nas-luci
 
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2 package/passwall2
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/OpenClash
-git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki package/nikki
-git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo package/OpenWrt-momo
-git clone --depth=1 -b master https://github.com/QiuSimons/luci-app-daed package/daed
-git clone --depth=1 -b master https://github.com/fw876/helloworld package/helloworld
+# 科学插件
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall  package/passwall
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2  package/passwall2
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages  package/passwall-packages
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash  package/OpenClash
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki  package/nikki
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo  package/momo
+git clone --depth=1 -b master https://github.com/QiuSimons/luci-app-daed  package/daed
+git clone --depth=1 -b master https://github.com/fw876/helloworld  package/helloworld
 
-# 插件添加
-git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog package/watchdog
-git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan package/taskplan
-git clone --depth=1 https://github.com/iv7777/luci-app-authshield package/authshield
-git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/easytier
-git clone --depth=1 https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community package/tailscale
-# 主题
-git clone --depth=1 -b openwrt-24.10 https://github.com/sbwml/luci-theme-argon.git package/argon
-git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora.git package/luci-theme-aurora
-git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config.git package/luci-app-aurora-config
-git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
-git clone --depth=1 -b master https://github.com/sirpdboy/luci-app-kucat-config.git package/luci-app-kucat-config
+# 功能插件
+git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog  package/watchdog
+git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan  package/taskplan
+git clone --depth=1 https://github.com/iv7777/luci-app-authshield  package/authshield
+git clone --depth=1 https://github.com/EasyTier/luci-app-easytier  package/easytier
+git clone --depth=1 https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community  package/tailscale
+
+# 主题（优化路径）
+git clone --depth=1 -b openwrt-24.10 https://github.com/sbwml/luci-theme-argon.git  package/argon
+git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora.git  package/aurora  
+git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config.git  package/aurora-config
+git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat.git  package/kucat
+git clone --depth=1 -b master https://github.com/sirpdboy/luci-app-kucat-config.git  package/kucat-config
