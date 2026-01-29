96ec5b6e26f2e2a4af36ec08e663ae8363265371
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-01-29 17:53:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,10 +18,9 @@
 echo "Removing broken Rust package..."
 rm -rf feeds/packages/lang/rust
 
-# 2. 克隆 ImmortalWrt 23.05 分支的 packages
-# 这个分支的 Rust 版本是 1.85.0 (稳定版)，下载源永久有效
+# 2. 克隆 ImmortalWrt 其他 分支的 packages
 echo "Cloning stable Rust from ImmortalWrt..."
-git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/packages.git temp_packages
+git clone --depth 1 -b openwrt-25.12 https://github.com/immortalwrt/packages.git temp_packages
 
 # 3. 偷梁换柱：把稳定的 Rust 搬进来
 cp -r temp_packages/lang/rust feeds/packages/lang/
