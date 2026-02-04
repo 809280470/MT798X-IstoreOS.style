515b1aff93c548a5a24a3c8323c9ed3b134f270d
809280470 <64239822+809280470@users.noreply.github.com>
2026-02-04 22:16:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,26 +16,26 @@ git clone --depth=1 https://github.com/linkease/nas-packages  package/nas
 git clone --depth=1 https://github.com/linkease/nas-packages-luci  package/nas-luci
 
 # 科学插件
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall  package/passwall
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2  package/passwall2
+# git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall  package/passwall
+# git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2  package/passwall2
 # git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages  package/passwall-packages
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash  package/OpenClash
-git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki  package/nikki
-git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo  package/momo
-git clone --depth=1 -b master https://github.com/QiuSimons/luci-app-daed  package/daed
-git clone --depth=1 -b master https://github.com/fw876/helloworld  package/helloworld
+# git clone --depth=1 -b master https://github.com/vernesong/OpenClash  package/OpenClash
+# git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki  package/nikki
+# git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo  package/momo
+# git clone --depth=1 -b master https://github.com/QiuSimons/luci-app-daed  package/daed
+# git clone --depth=1 -b master https://github.com/fw876/helloworld  package/helloworld
 
 # 功能插件
-git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog package/watchdog
+# git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog package/watchdog
 git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan package/taskplan
 git clone --depth=1 https://github.com/iv7777/luci-app-authshield package/authshield
 git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/easytier
 git clone https://github.com/stackia/rtp2httpd.git package/rtp2httpd
-git clone --depth=1 https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community package/tailscale
+# git clone --depth=1 https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community package/tailscale
 
 # 主题
-git clone --depth=1 -b openwrt-24.10 https://github.com/sbwml/luci-theme-argon package/argon
-git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora package/aurora  
-git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config package/aurora-config
-git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat package/kucat
-git clone --depth=1 -b master https://github.com/sirpdboy/luci-app-kucat-config package/kucat-config
+# git clone --depth=1 -b openwrt-24.10 https://github.com/sbwml/luci-theme-argon package/argon
+# git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora package/aurora  
+# git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config package/aurora-config
+# git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat package/kucat
+# git clone --depth=1 -b master https://github.com/sirpdboy/luci-app-kucat-config package/kucat-config
