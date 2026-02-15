9288010c85ed53e2aea231128a77fceaf47c9da8
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-02-15 14:40:16 +0800

    Create diy-part4.sh

---
+++ diy-part4.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part4.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# 功能插件
+git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog package/watchdog
+git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan package/taskplan
+git clone --depth=1 https://github.com/iv7777/luci-app-authshield package/authshield
+git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/easytier
+git clone --depth=1 https://github.com/selfcan/luci-app-tailscale package/tailscale
+
+# 添加 openwrt 18.06-21.02 插件库
+git clone --depth=1 -b Immortalwrt https://github.com/makebl/openwrt-package  package/openwrt-package
