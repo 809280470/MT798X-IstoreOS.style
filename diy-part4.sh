8bbbc07de5b1e0f0efd4835f7178e07391130fc2
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-02-15 15:15:08 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -2,7 +2,7 @@
 #
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part4.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Description: OpenWrt DIY script part 4 (Before Update feeds)
 #
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
