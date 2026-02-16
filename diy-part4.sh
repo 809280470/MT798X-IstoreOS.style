37d281bd8793856f081506e514ea9fdc448aa5d5
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-02-16 11:43:02 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -12,11 +12,11 @@
 
 # 功能插件
 git clone --depth=1 https://github.com/iv7777/luci-app-authshield package/authshield
-git clone --depth=1 https://github.com/asvow/luci-app-tailscale package/tailscale
+# git clone --depth=1 https://github.com/asvow/luci-app-tailscale package/tailscale
 
 # 添加 openwrt 18.06-21.02 插件库
 # git clone --depth=1 -b Immortalwrt https://github.com/makebl/openwrt-package  package/openwrt-package
-git clone --depth=1 -b Immortalwrt https://github.com/shidahuilang/openwrt-package  package/openwrt-package
+# git clone --depth=1 -b Immortalwrt https://github.com/shidahuilang/openwrt-package  package/openwrt-package
 
 # theme
 git clone --depth=1 -b openwrt-23.05 https://github.com/sbwml/luci-theme-argon package/argon
