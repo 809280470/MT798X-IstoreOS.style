3176d1547c110e32e91338c90a5a681e9a328c4c
809280470 <64239822+809280470@users.noreply.github.com>
2026-02-20 01:28:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -32,10 +32,4 @@ git clone --depth=1 https://github.com/iv7777/luci-app-authshield package/authsh
 git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/easytier
 git clone --depth=1 https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community package/tailscale
 git clone --depth=1 https://github.com/isalikai/luci-app-owq-wol package/luci-app-owq-wol
-
-# 主题
-git clone --depth=1 -b openwrt-24.10 https://github.com/sbwml/luci-theme-argon package/argon
-git clone --depth=1 https://github.com/eamonxg/luci-theme-aurora package/aurora
-git clone --depth=1 https://github.com/eamonxg/luci-app-aurora-config package/aurora-config
-git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat package/kucat
-git clone --depth=1 -b master https://github.com/sirpdboy/luci-app-kucat-config package/kucat-config
+git clone --depth=1 https://github.com/stackia/rtp2httpd.git package/rtp2httpd
